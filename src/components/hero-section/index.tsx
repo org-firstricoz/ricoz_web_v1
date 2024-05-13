0df268506c78526a96d31a3c36a42fbaaabc6f91
unknown <xlongclaw@gmail.com>
2024-05-13 19:36:59 +0530

    chore: change bgImg URL to cloudinary url

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -2,7 +2,7 @@ import BlueButtonInput from "../blue-button-input";
 
 export default function HeroSection() {
   return (
-    <div className="text-white flex bg-[url(hero-bg.jpeg)] bg-cover relative">
+    <div className="text-white flex bg-[url(https://res.cloudinary.com/dlxpf7d8c/image/upload/v1715609111/ddjuet3erd6ze2wqdccy.jpg)] bg-cover relative">
       <div className="bg-rz-darkgreen h-full px-32 pt-40 pb-72 w-full">
         <h2 className="text-6xl font-bold max-w-[50rem] mb-8">
           Designing Dreams, Crafting Brands
