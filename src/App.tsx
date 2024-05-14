c2351e1804a8298258daa6f45ffe1850596a5641
unknown <xlongclaw@gmail.com>
2024-05-14 23:07:37 +0530

    fix: configure routes without browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import LandingPage from "./screens/LandingPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AboutPage from "./screens/AboutPage";
 
 export default function App(): React.JSX.Element {
   return (
     <div className="">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/about" element={<AboutPage/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
