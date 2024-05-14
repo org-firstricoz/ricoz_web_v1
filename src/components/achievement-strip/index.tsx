42a063eb973cddffc5eb1f0c17c410276162a84f
unknown <xlongclaw@gmail.com>
2024-05-14 22:54:41 +0530

    fix: resolve unused index issue while deploy

--- src/components/achievement-strip/index.tsx
+++ src/components/achievement-strip/index.tsx
@@ -4,7 +4,7 @@ import ACHIEVEMENTS from "./constants";
 export default function AchievementStrip() {
   return (
     <div className="bg-rz-lategray flex gap-16 justify-center">
-      {ACHIEVEMENTS.map((achievement, i) => (
+      {ACHIEVEMENTS.map((achievement) => (
         <AchievementWrapper
         key={`${achievement.title}_${achievement.value}`}
           img={achievement.img}
