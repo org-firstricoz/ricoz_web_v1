1066d2f25a0d7e683f487b1f6b1baececefa4ff4
unknown <xlongclaw@gmail.com>
2024-05-14 23:15:02 +0530

    fix: configure routes with browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import LandingPage from "./screens/LandingPage";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutPage from "./screens/AboutPage";
 
 export default function App(): React.JSX.Element {
   return (
-    <div className="">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
