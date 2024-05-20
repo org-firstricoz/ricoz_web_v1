1fcacc937cd462402e4c46e82e1ce36fce56baec
unknown <xlongclaw@gmail.com>
2024-05-20 22:11:21 +0530

    fix: positioning issue in hamburger menu

--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -13,11 +13,11 @@ export default function Menu() {
   };
   return (
     <div className="flex sm:hidden relative">
-      <button onBlur={handleMenuExpandOnBlur} onClick={handleMenuExpandOnClick}>
+      <button className="focus:outline-none border-none" onBlur={handleMenuExpandOnBlur} onClick={handleMenuExpandOnClick}>
         <MenuIcon />
       </button>
       {expand && (
-        <div className="flex flex-col absolute right-0 top-12 text-black w-36 bg-white rounded-xl items-end border border-black">
+        <div className="flex flex-col absolute z-30 right-0 top-12 text-black w-36 bg-white rounded-xl items-end border border-black">
           {NAVLINKS.map((navlink,i) => (
             <a key={`NAVLINK_MENU_${navlink.title}_${i}`} href="#" className="px-4 py-2">
               {textEllipsis(navlink.title, 12)}
