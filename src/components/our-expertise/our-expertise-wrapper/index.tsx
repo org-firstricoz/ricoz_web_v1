c9400ea7098b37c146a4b8aa1b2f5958da55b311
unknown <xlongclaw@gmail.com>
2024-05-29 22:08:59 +0530

    fix: remove image in expertise wrapper

--- src/components/our-expertise/our-expertise-wrapper/index.tsx
+++ src/components/our-expertise/our-expertise-wrapper/index.tsx
@@ -15,11 +15,11 @@ export default function OutExpertiseWrapper({
 }) {
   return (
     <div className="flex flex-col bg-green-200/20 items-start rounded-lg p-4 gap-4">
-      <img className="w-16" src={img} alt="" />
+      {/* <img className="w-16" src={img} alt="" /> */}
       <div className="">{subtitle}</div>
       <div className="mb-4">
         <div className="font-semibold text-xl">{title}</div>
-        <div className="tracking-[1px] mt-1 text-black/60 font-medium text-sm sm:text-base lg:text-sm xl:text-base ">{des}</div>
+        <div className="mt-3 text-black/60 font-medium text-sm sm:text-base lg:text-sm xl:text-base ">{des}</div>
       </div>
       <OurExpertiseButton buttonTitle={buttonTitle} />
     </div>
