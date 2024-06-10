fbb7b3d44c2396019a99f27c43e3928347df75a3
unknown <xlongclaw@gmail.com>
2024-06-10 22:36:12 +0530

    fix: deployment error, unused variables and type for ract-scroll

--- src/components/svgs/PaperPlane.tsx
+++ src/components/svgs/PaperPlane.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const PaperPlane = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" width="58" height="58">
     <path d="M2.01 21L23 12 2.01 3v7.01L17 12 2.01 16.02z" fill="#657968"/>
